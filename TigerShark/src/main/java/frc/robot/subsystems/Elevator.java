0cf444e6614528c2a46d81fd2e4a29e30562b9ff
SPradi <106702747+SPtigertrons@users.noreply.github.com>
2025-02-11 15:47:15 -0600

    Elevator

--- TigerShark/src/main/java/frc/robot/subsystems/Elevator.java
+++ TigerShark/src/main/java/frc/robot/subsystems/Elevator.java
@@ -6,9 +6,23 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Elevator extends SubsystemBase {
-  /** Creates a new Elevator. */
-  public Elevator() {}
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
+public class elevator extends SubsystemBase {
+
+  SparkMax m_elevator;
+  /** Creates a new elevator. */
+  public elevator() {
+    m_elevator = new SparkMax(0, MotorType.kBrushless);
+    
+    SparkMaxConfig elevatorConfig = new SparkMaxConfig();
+    
+  }
 
   @Override
   public void periodic() {
