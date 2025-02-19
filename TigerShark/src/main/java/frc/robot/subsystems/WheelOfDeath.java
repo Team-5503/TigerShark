5130182f3f9a2de95596140c9387cd5cd746fdec
SPradi <106702747+SPtigertrons@users.noreply.github.com>
2025-02-18 22:29:18 -0600

    Update WheelOfDeath.java

--- TigerShark/src/main/java/frc/robot/subsystems/WheelOfDeath.java
+++ TigerShark/src/main/java/frc/robot/subsystems/WheelOfDeath.java
@@ -4,17 +4,46 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.controller.ArmFeedforward;
 
 public class WheelOfDeath extends SubsystemBase {
   /** Creates a new WheelOfDeath. */
-  public WheelOfDeath() {}
+  SparkMax mainWheelMotor, coralMotor, algaeMotor;
+  AbsoluteEncoder mainWheelEncoder;
+  public WheelOfDeath() 
+  {
+    mainWheelMotor = new SparkMax(15, MotorType.kBrushless);
+    coralMotor = new SparkMax(16, MotorType.kBrushless);
+    algaeMotor = new SparkMax(17, MotorType.kBrushless);
+    mainWheelEncoder = mainWheelMotor.getAbsoluteEncoder();
+    
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+   //fix this
+  public double getWheelPositionRaw(){
+    return mainWheelEncoder.getPosition();
+  }
+
+  public double getWheelPositionAngle(){
+   
+    return mainWheelEncoder.getPosition()*360;
+  }
+  
 }
